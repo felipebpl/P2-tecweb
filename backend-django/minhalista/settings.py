645d024a8d04cc9e5052e91f838ba6988f74e77e
Felipe <felipebpl@al.insper.edu.br>
2021-12-01 23:48:36 -0300

    backend rodando com o sqlite

--- backend-django/minhalista/settings.py
+++ backend-django/minhalista/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'minhalista.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'getit',
-        'USER': 'getituser',
-        'PASSWORD': 'getitsenha',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
